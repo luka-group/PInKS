6d114b50ecc5c412789fc8b54e40556bcb18cb61
piyushkhanna00705 <piyushkhanna00705@gmail.com>
2021-07-02 17:44:42 +0530

    Add files via upload

--- Scripts/grep_unless.sh
+++ Scripts/grep_unless.sh
@@ -1,10 +1,2 @@
-#grep -i "[\w\-\\\/\+\* ,']*unless[\w\-\\\/\+\* ,']*" /nas/home/qasemi/CQplus/Outputs/Corpora/OpenWebText/openwebtext/urlsf_subset00-100_data -a
 file_path="$1"
-# message='ACTION unless CONDITION.'
-#pattern="[\w\-\\\/\+\* ,\']*"
-# pattern="[\w]*"
-# no_action="${message//ACTION/${pattern}}"
-# final="${no_action//CONDITION/${pattern}}"
-# echo "${final}"
-
 grep -i -c 'unless' "$file_path"
