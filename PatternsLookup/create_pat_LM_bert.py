939fa9ffe3751e0f27b91f2e9cb2c3e88d2bab8a
Piyush Khanna <piyushkhanna00705@gmail.com>
2021-08-05 17:46:29 +0530

    Add files via upload

---
+++ PatternsLookup/create_pat_LM_bert.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  4 19:58:07 2021
+
+@author: Dell
+"""
+
+
+from transformers import pipeline
+
+
+import logging
+logger = logging.getLogger(__name__)
+import IPython
+import hydra
+import omegaconf
+import pandas as pd 
+
+import json  
+
+
+unmasker = pipeline('fill-mask', model='bert-base-uncased')
+
+all_conj={'only if', 'contingent upon', 'if',"in case", "in the case that", "in the event", "on condition", "on the assumption",
+            "on these terms",  "supposing", "with the proviso",
+            "except", "except for", "excepting that", "if not", "lest",  "without"}
+
+possible_replacements={}
+
+
+for conj in all_conj:
+    possible_replacements[conj]={}
+
+def addMaskedConj(sent):
+    for conj in all_conj:
+        if conj in sent:
+            sent=sent.replace(conj,"[MASK]")
+            unmasked_words=list(unmasker(sent))
+            for new_word in unmasked_words[:3]:
+                if new_word['token_str']!=conj:
+                    possible_replacements[conj].add(new_word['token_str'])
+            
+
+
+@hydra.main(config_path="../Configs", config_name="create_pat_LM_config")
+def main(config: omegaconf.dictconfig.DictConfig):
+    df=pd.read_csv(config.corpus_path)
+    for index,row in df.iterrows():
+        addMaskedConj(row['text'])
+    with open(config.output_name, "w") as outfile: 
+        json.dump(possible_replacements, outfile)
+        
+        
+        
+        
