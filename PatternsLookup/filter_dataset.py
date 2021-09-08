3de980903dcead3bbfdbb03a902f22b1a61e7f26
Piyush Khanna <piyushkhanna00705@gmail.com>
2021-09-08 22:11:48 +0530

    Add files via upload

--- PatternsLookup/filter_dataset.py
+++ PatternsLookup/filter_dataset.py
@@ -33,6 +33,42 @@ VERB_CODES = {
     'VBZ',  # Verb, 3rd person singular present
 }
 
+
+def pattern_exists(pattern,line):
+    pattern_keys = re.findall(r'\{([^\}]+)}', pattern)
+    replacements = {k: REPLACEMENT_REGEX[k] for k in pattern_keys}    
+    regex_pattern = pattern.format(**replacements)
+    m_list = re.findall(regex_pattern, line)
+    
+    # print(pattern_keys)
+    # print(replacements)
+    # print(regex_pattern)
+    # print(m_list)
+    
+    for m in m_list:
+        match_full_sent = line
+        for sent in line:
+            if all([ps in sent for ps in m]):
+                match_full_sent = sent
+    
+        match_dict = dict(zip(pattern_keys, m))
+        if 'negative_precondition' in pattern_keys:
+                    if any([nw in match_dict['negative_precondition'] for nw in PatternUtils.NEGATIVE_WORDS]):
+                        return True
+                    else:
+                        return False
+
+        # if 'negative_action' in pattern_keys:
+        #     if any([nw in match_dict['negative_action'] for nw in PatternUtils.NEGATIVE_WORDS]):
+        #         return True
+        #     else:
+        #         return False
+    if len(m_list)>0:
+        return True
+    return False
+
+
+
 def isQuestion(text):
     text=text.strip()
     if text[-1]=='?' or text.split()[0].lower() in question_start_words:
@@ -54,6 +90,24 @@ def isEnglish(text):
     return False
 
 
+def containsIf(text):
+    if_not_pat="{action} if not {precondition}"
+    if pattern_exists(if_not_pat,text):
+        return False
+    elif pattern_exists("{action} if {precondition}",text):
+        return True
+    return False
+    
+
+def containsBut(text):
+    pat="{action} but {precondition}"
+    if pattern_exists(pat,text):
+        return True
+    return False
+    
+
+
+
 @hydra.main(config_path="../Configs", config_name="filter_dataset_config")
 def main(config: omegaconf.dictconfig.DictConfig):
     merged_df=pd.read_csv(config.merged_dataset)
@@ -62,13 +116,18 @@ def main(config: omegaconf.dictconfig.DictConfig):
     filtered_dataset=pd.DataFrame(columns=column_names)
     count=0
     for index,row in tqdm(merged_df.iterrows()):
-        if not(isQuestion(row['text'])) and hasVerb(row['precondition']) and isEnglish(row['text']):
+        if not(isQuestion(row['text'])) and hasVerb(row['precondition']) and isEnglish(row['text']) and !containsIf(row['text']) and !containsBut(row['text']):
             new_row = {"text": row['text'], "action": row['action'], "precondition": row['precondition'], "label":row['label']}
             filtered_dataset = filtered_dataset.append(new_row, ignore_index = True)
             count+=1
     # print("Filtered True count="+str(count))
     print("Filtered len="+str(len(filtered_dataset)))
     filtered_dataset.to_csv(config.output_path)
+    
+    count = filtered_dataset["label"].value_counts()
+    print("Label  Count")
+    print(count)
+    
 
 if __name__ == '__main__':
     main()
