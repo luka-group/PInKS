882f302d12e0ffceb9f10c4db297110e9fcd5574
Piyush Khanna <piyushkhanna00705@gmail.com>
2021-09-19 13:11:03 +0530

    refactoring SnorkelUtil

--- PatternsLookup/process_omcs_using_snorkel.py
+++ PatternsLookup/process_omcs_using_snorkel.py
@@ -36,6 +36,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+from SnorkelUtil import SnorkelUtil
+
+
 
 
 FACT_REGEX = r'([a-zA-Z0-9_\-\\\/\+\* \'"’%]{10,})'
@@ -373,38 +376,19 @@ def addActionPrecondition(L, LFA_df, df):
 
 @hydra.main(config_path="../Configs", config_name="snorkel_config")
 def main(config: omegaconf.dictconfig.DictConfig):
+    """With SnorkelUtil"""
     omcs_df = pd.read_csv(config.corpus_path, sep="\t", error_bad_lines=False)
     omcs_df['text'] = omcs_df['text'].astype(str)
-    
-    applier = PandasLFApplier(lfs)
-    # global L_omcs
-    L_omcs = applier.apply(omcs_df)
-    
-    # print(type(L_omcs))
-    # print(L_omcs)
-    
-    
-    # L_matrix=np.copy(L_omcs)
-    
-    print(LFAnalysis(L_omcs, lfs).lf_summary())
-    print(type(LFAnalysis(L_omcs, lfs).lf_summary()))
-    
-    
-    LFA_df=LFAnalysis(L_omcs, lfs).lf_summary().copy()
-    
-    examples_df=returnExamples(L_omcs, LFA_df, omcs_df)
+
+    L_omcs, LFA_df=SnorkelUtil(omcs_df)
+    examples_df=SnorkelUtil.returnExamples(L_omcs, LFA_df, omcs_df)
     examples_df.to_csv(config.output_examples)
     
-    
-    # Train the label model and compute the training labels
     label_model = LabelModel(cardinality=3, verbose=True)
     label_model.fit(L_omcs, n_epochs=config.snorkel_epochs, log_freq=50, seed=123)
     omcs_df["label"] = label_model.predict(L=L_omcs, tie_break_policy="abstain")
     
-    
-    omcs_df=addActionPrecondition(L_omcs, LFA_df, omcs_df)
-    
-    
+    omcs_df=SnorkelUtil.addActionPrecondition(L_omcs, LFA_df, omcs_df)
     omcs_df = omcs_df[omcs_df.label != ABSTAIN]
     
     omcs_df.to_csv(config.output_name)
@@ -413,9 +397,62 @@ def main(config: omegaconf.dictconfig.DictConfig):
     print("Label  Count")
     print(count)
     
-    
     with open('LabelingMatrix.npy', 'wb') as f:
         np.save(f, L_omcs)
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    """Without SnorkelUtil"""
+    # omcs_df = pd.read_csv(config.corpus_path, sep="\t", error_bad_lines=False)
+    # omcs_df['text'] = omcs_df['text'].astype(str)
+    
+    # applier = PandasLFApplier(lfs)
+    # # global L_omcs
+    # L_omcs = applier.apply(omcs_df)
+    
+    # # print(type(L_omcs))
+    # # print(L_omcs)
+    
+    
+    # # L_matrix=np.copy(L_omcs)
+    
+    # print(LFAnalysis(L_omcs, lfs).lf_summary())
+    # print(type(LFAnalysis(L_omcs, lfs).lf_summary()))
+    
+    
+    # LFA_df=LFAnalysis(L_omcs, lfs).lf_summary().copy()
+    
+    # examples_df=returnExamples(L_omcs, LFA_df, omcs_df)
+    # examples_df.to_csv(config.output_examples)
+    
+    
+    # # Train the label model and compute the training labels
+    # label_model = LabelModel(cardinality=3, verbose=True)
+    # label_model.fit(L_omcs, n_epochs=config.snorkel_epochs, log_freq=50, seed=123)
+    # omcs_df["label"] = label_model.predict(L=L_omcs, tie_break_policy="abstain")
+    
+    # omcs_df=addActionPrecondition(L_omcs, LFA_df, omcs_df)
+    
+    
+    # omcs_df = omcs_df[omcs_df.label != ABSTAIN]
+    
+    # omcs_df.to_csv(config.output_name)
+    
+    # count = omcs_df["label"].value_counts()
+    # print("Label  Count")
+    # print(count)
+    
+    
+    # with open('LabelingMatrix.npy', 'wb') as f:
+    #     np.save(f, L_omcs)
     
 
 
