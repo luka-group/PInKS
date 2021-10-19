9f992ca5ed6f7e06b60644e70d92e77e91b8966a
Piyush Khanna <piyushkhanna00705@gmail.com>
2021-10-19 23:12:15 +0530

    fine-tuning mod_lm

--- Scripts/run_modified_lm_tuning.sh
+++ Scripts/run_modified_lm_tuning.sh
@@ -7,4 +7,4 @@ export PYTHONPATH="$(pwd)"
 python Models/ModifiedLangModeling.py \
     username=$(whoami) \
     lm_module.model_name_or_path="roberta-large" \
-    data_module.train_file="/nas/home/${username}/CQplus/Outputs/process_dataset_using_snorkel/0.7/filtered_dataset.csv"
+    data_module.train_file="/nas/home/$(whoami)/CQplus/Outputs/process_dataset_using_snorkel/0.7/filtered_dataset.csv"
