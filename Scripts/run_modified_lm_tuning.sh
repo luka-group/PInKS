8008ef1efbe09b6cfcd7f1c6e638658aecbcfb08
Piyush Khanna <piyushkhanna00705@gmail.com>
2021-10-28 01:56:06 +0530

    fixing ray-tune tuning

--- Scripts/run_modified_lm_tuning.sh
+++ Scripts/run_modified_lm_tuning.sh
@@ -5,7 +5,7 @@ cd ../Models
 # export PYTHONPATH="$(pwd)"
 
 
-python Models/ModifiedLangModeling.py \
+python ModifiedLangModeling.py \
     username=$(whoami) \
     lm_module.model_name_or_path="roberta-large" \
     data_module.train_file="/nas/home/$(whoami)/CQplus/Outputs/process_dataset_using_snorkel/0.7/filtered_dataset.csv"
