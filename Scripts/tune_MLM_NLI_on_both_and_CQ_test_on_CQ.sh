9b060a94f3949a9baf79f466cef3e2202a61c048
proska <qasemi.ehs@gmail.com>
2021-11-09 20:08:50 -0800

    Updates:  - fix the run script inconsistency with new version

--- Scripts/tune_MLM_NLI_on_both_and_CQ_test_on_CQ.sh
+++ Scripts/tune_MLM_NLI_on_both_and_CQ_test_on_CQ.sh
@@ -6,9 +6,8 @@ export PYTHONPATH="$(pwd)"
 python Models/Tune_Eval_NLI.py \
     model_setup.model_name="roberta-large-mnli" \
     model_setup.tuned_model_path="/nas/home/pkhanna/CQplus/Outputs/ModifiedLangModeling/Checkpoint/ModifiedLMModule.ckpt" \
-    +n_MNLI_samples='40000'\
     +nli_module_class='NLIModuleWithTunedLM' \
-    data_module.train_composition=[weakcq,cq] \
+    data_module.train_composition=[mnli,weakcq] \
     data_module.test_composition=[cq] \
     +weakcq_recal_threshold=0.90 \
     +n_weakcq_samples=100000 \
