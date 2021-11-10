f4a57f06065c2dfae0e79372cc3ec2625efd67de
proska <qasemi.ehs@gmail.com>
2021-11-09 20:08:00 -0800

    Updates:  - fix the run script inconsistency with new version

--- Scripts/tune_MLM_NLI_on_both_and_CQ_test_on_CQ.sh
+++ Scripts/tune_MLM_NLI_on_both_and_CQ_test_on_CQ.sh
@@ -8,7 +8,16 @@ python Models/Tune_Eval_NLI.py \
     model_setup.tuned_model_path="/nas/home/pkhanna/CQplus/Outputs/ModifiedLangModeling/Checkpoint/ModifiedLMModule.ckpt" \
     +n_MNLI_samples='40000'\
     +nli_module_class='NLIModuleWithTunedLM' \
-    +data_class=["BaseNLIDataModule","CQOnlyNLIDataModule"] \
+    data_module.train_composition=[weakcq,cq] \
+    data_module.test_composition=[cq] \
+    +weakcq_recal_threshold=0.90 \
+    +n_weakcq_samples=100000 \
+    +n_anion_samples=100000 \
+    +n_winoventi_samples=100000 \
+    +n_atomic_samples=100000 \
+    +n_mnli_samples=100000 \
+    +n_cq_samples=500 \
+    data_module.use_class_weights=true \
     train_setup.do_train=true \
     hardware.gpus='2' \
     train_setup.batch_size=8 \
